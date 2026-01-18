295b5c3f9606f1ab1d5c8cd63a90994cc7da6ab6
alibhojani92 <alibhojani92@gmail.com>
2026-01-18 17:00:43 +0530

    Update dailyTest.flow.js

--- src/bot/flows/dailyTest.flow.js
+++ src/bot/flows/dailyTest.flow.js
@@ -2,10 +2,23 @@ import { getDailyTestMCQs } from "../../logic/test.engine.js";
 import { testKeyboard } from "../keyboards/test.keyboard.js";
 
 export async function dailyTestFlow(update, env) {
-  const chatId = update.callback_query.message.chat.id;
-  const userId = update.callback_query.from.id;
+  const chatId =
+    update.message?.chat?.id ||
+    update.callback_query?.message?.chat?.id;
+
+  const userId =
+    update.message?.from?.id ||
+    update.callback_query?.from?.id;
 
   const mcq = await getDailyTestMCQs(env.DB, userId);
+
+  if (!mcq) {
+    return {
+      method: "sendMessage",
+      chat_id: chatId,
+      text: "No questions available"
+    };
+  }
 
   return {
     method: "sendMessage",
