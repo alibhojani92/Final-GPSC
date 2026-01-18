92df90ea93e9305d5aaf04348dd717410fd429d9
alibhojani92 <alibhojani92@gmail.com>
2026-01-18 15:25:56 +0530

    Create subjects.js

---
+++ src/constants/subjects.js
@@ -0,0 +1,9 @@
+export const SUBJECTS = [
+  { id: "PHARMA", name: "Pharmacology" },
+  { id: "PATH", name: "General Pathology & Microbiology" },
+  { id: "OMP", name: "Oral Medicine & Radiology" },
+  { id: "OMFS", name: "Oral & Maxillofacial Surgery" },
+  { id: "PERIO", name: "Periodontology" },
+  { id: "CONS", name: "Conservative Dentistry & Endodontics" },
+  { id: "OPM", name: "Oral Pathology & Oral Microbiology" }
+];
