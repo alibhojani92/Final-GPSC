a150962b88c787b31029949be2aec2567f72d858
alibhojani92 <alibhojani92@gmail.com>
2026-01-18 15:23:05 +0530

    Create start.flow.js

---
+++ src/bot/flows/start.flow.js
@@ -0,0 +1,12 @@
+import { mainKeyboard } from "../keyboards/main.keyboard.js";
+
+export async function startFlow(update, env) {
+  const chatId = update.message.chat.id;
+
+  return {
+    method: "sendMessage",
+    chat_id: chatId,
+    text: "Welcome to Dental Pulse 18 Test Bot\n\nSelect an option below:",
+    reply_markup: mainKeyboard()
+  };
+}
