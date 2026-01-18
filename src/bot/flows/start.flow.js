f18be44556ddd43ce90a3a51bf61bd1dd6d35595
alibhojani92 <alibhojani92@gmail.com>
2026-01-18 17:51:05 +0530

    Update start.flow.js

--- src/bot/flows/start.flow.js
+++ src/bot/flows/start.flow.js
@@ -1,14 +1,12 @@
 import { mainKeyboard } from "../keyboards/main.keyboard.js";
 
 export async function startFlow(update, env) {
-  const chatId =
-    update.message?.chat?.id ||
-    update.callback_query?.message?.chat?.id;
+  const chatId = update.message.chat.id;
 
   return {
     method: "sendMessage",
     chat_id: chatId,
-    text: "Welcome to Dental Pulse 18 Test Bot\n\nSelect an option below:",
+    text: "Welcome Dr Arzoo❤️\n\nSelect an option below:",
     reply_markup: mainKeyboard()
   };
 }
